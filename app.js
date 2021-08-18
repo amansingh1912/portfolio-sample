9e30866f3e21822827132913f39a03ea3eaf6bb9
Aman Kumar Singh <72240938+amansingh1912@users.noreply.github.com>
2021-08-18 20:14:37 +0530

    portfolio

---
+++ app.js
@@ -0,0 +1,6 @@
+// imgMain = document.querySelector(".img-sec");
+// imgIn = document.querySelector(".image-sec");
+
+// imgMain.addEventListener("click", function() {
+//     imgIn.classList.add(".show-img");
+// })
